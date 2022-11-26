95e2a640a2774ea83b9b80677d56db2b1b900c80
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2022-11-26 13:11:16 -0500

    enlever fichier de test

--- testMIME.js
+++
@@ -1,5 +0,0 @@
-const MIMEType = require("./MIMEType.js"); //module qui export un objet des extension et MIME Type
-let extension = ".html";
-
-console.log(`extension ${extension}`);
-console.log(MIMEType[extension]);
